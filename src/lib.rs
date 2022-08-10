0b17c660df1e6f3ff1d3d44fdf8c0f2920de110a
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2022-08-10 10:34:28 -0700

    create prototype working version of welford's online algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#[derive(Default)]
+pub struct ExistingAggregate {
+    pub count: f64,
+    pub mean: f64,
+    pub m2: f64,
+}
+
+impl ExistingAggregate {
+    pub fn new() -> ExistingAggregate {
+        Default::default()
+    }
+
+    pub fn update(&mut self, new_value: f64) -> &mut ExistingAggregate {
+        self.count += 1.0;
+        let delta = new_value - self.mean;
+        self.mean += delta / self.count;
+        let delta2 = new_value - self.mean;
+        self.m2 += delta * delta2;
+        self
+    }
+
+    pub fn finalize(&self) -> Option<(f64, f64, f64)> {
+        if self.count < 2.0 {
+            None
+        } else {
+            let mean = self.mean;
+            let variance = self.m2 / self.count;
+            let sample_variance = self.m2 / (self.count - 1.0);
+            Some((mean, variance, sample_variance))
+        }
     }
 }
