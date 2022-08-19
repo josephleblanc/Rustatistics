b1b8ba8efde17b2301c45b80e74c841c502e1941
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2022-08-18 17:12:12 -0700

    add function for rolling_mean with variable window size

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,32 @@ pub fn mean_and_variance(data: &[f64]) -> Option<(f64, f64, f64)> {
     exag.finalize()
 }
 
+// Calculates the rolling mean for an array of f64.
+// Because it is calculating over a range of window_size elements, the first
+// window_size number of elements will not be calculated, and will be None.
+pub fn rolling_mean(data: &[f64], window_size: usize) -> Vec<Option<f64>> {
+    if window_size < 2 {
+        panic!("window_size cannot be smaller than 2 in function rolling_mean");
+    }
+    if window_size > data.len() {
+        panic!("window_size cannot be larger than data.len() in function rolling_mean");
+    }
+
+    let mut means_vec: Vec<Option<f64>> = vec![None; window_size - 1];
+    let mut sum = data
+        .iter()
+        .zip(0..window_size)
+        .fold(0.0_f64, |acc, (x, _)| acc + x);
+    let window_size_f64 = window_size as f64;
+    means_vec.push(Some(sum / window_size_f64));
+
+    for i in window_size..data.len() {
+        sum = sum - data[i - window_size] + data[i];
+        means_vec.push(Some(sum / window_size_f64));
+    }
+    means_vec
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -81,5 +107,24 @@ mod tests {
         assert_eq!(1.0, mean);
         assert_eq!(0.0, variance);
         assert_eq!(0.0, sample_variance);
+    }
+
+    #[test]
+    fn simple_rolling_mean() {
+        let data: Vec<f64> = (10..=70).step_by(10).map(|n| n as f64).collect();
+        let window_size = 3;
+        let means_vec = rolling_mean(&data, window_size);
+        assert_eq!(
+            vec![
+                None,
+                None,
+                Some(20.0),
+                Some(30.0),
+                Some(40.0),
+                Some(50.0),
+                Some(60.0)
+            ],
+            means_vec
+        );
     }
 }
